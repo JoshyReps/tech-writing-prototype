037dc6d6746a40779d4c5772cc1d5beac47a9eac
JoshyReps <201358668+JoshyReps@users.noreply.github.com>
2026-05-24 00:16:47 +0800

    fixed back container

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,7 +4,7 @@ import Logout from "../Features/Authentication/Logout";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 flex h-18 w-full items-center justify-between bg-olive-300 px-3 py-2 shadow-md shadow-black/20">
+    <nav className="fixed top-0 left-0 z-100 flex h-18 w-full items-center justify-between bg-olive-300 px-3 py-2 shadow-md shadow-black/20">
       <div className="flex h-12 w-28 items-center gap-2">
         <Image src="logo.svg" alt="logo" />
         <TextLogo />
